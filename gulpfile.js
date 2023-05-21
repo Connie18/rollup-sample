2adc6deb38a001e9ee8929d30fa52097268f86b2
Yuichiro Kondo <kondo_u16@yahoo.co.jp>
2023-05-21 23:21:24 +0900

    Update concatTS

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ const tsMorph = require("ts-morph");
 const fs = require("fs");
 const path = require("path");
 
-const getImportFilePaths = (project, filePath, filePaths = []) => {
+// This function will recursively get the file paths of the imports
+const resolveImports = (project, filePath, filePaths = []) => {
   // Skip if the file has already been processed
   if (filePaths.includes(filePath)) {
-    console.warn(`Warning: Circular import detected at ${filePath}`);
     return filePaths;
   }
 
@@ -17,49 +17,67 @@ const getImportFilePaths = (project, filePath, filePaths = []) => {
       path.dirname(filePath),
       importDeclaration.getModuleSpecifierValue() + ".ts"
     );
-    getImportFilePaths(project, importFilePath, filePaths);
+    resolveImports(project, importFilePath, filePaths);
   });
 
+  // Push the file path into the list after processing its imports
   filePaths.push(filePath);
 
   return filePaths;
 };
 
+// Configuration object
+const config = {
+  tsConfigFilePath: "tsconfig.json",
+  entryFilePath: ["src/index.ts", "src/components/Car.ts"],
+  outputDir: "dist",
+};
+
+// This is the main Gulp task
 const concatTS = function (done) {
-  const project = new tsMorph.Project({ tsConfigFilePath: "tsconfig.json" });
-  const entryFilePath = "src/index.ts";
-  const outputFile = path.join("dist", "output.js");
-
-  let outputText = "";
-
-  // Recursively resolve the imports starting from the entry file
-  const inputFilePaths = getImportFilePaths(project, entryFilePath);
-
-  for (const inputFilePath of inputFilePaths) {
-    const sourceFile = project.addSourceFileAtPath(inputFilePath);
-
-    // Remove all import declarations
-    sourceFile.getImportDeclarations().forEach((id) => id.remove());
-
-    // Remove 'export' keyword from classes, functions, variables etc.
-    sourceFile.getClasses().forEach((c) => {
-      if (c.isExported()) {
-        c.setIsDefaultExport(false);
-        c.setIsExported(false);
-      }
+  // Iterate over each entry file
+  config.entryFilePath.forEach((entryFilePath) => {
+    const project = new tsMorph.Project({
+      tsConfigFilePath: config.tsConfigFilePath,
     });
 
-    // Save the updated source file text in memory
-    outputText += sourceFile.getFullText() + "\n";
-  }
+    let outputText = "";
 
-  // Transpile the TypeScript code to JavaScript
-  const outputJS = tsMorph.ts.transpileModule(outputText, {
-    compilerOptions: project.getCompilerOptions(),
+    // Recursively resolve the imports starting from the entry file
+    const inputFilePaths = resolveImports(project, entryFilePath);
+
+    // Iterate over the files to be concatenated
+    for (const inputFilePath of inputFilePaths) {
+      const sourceFile = project.getSourceFile(inputFilePath);
+
+      // Remove all import declarations
+      sourceFile.getImportDeclarations().forEach((id) => id.remove());
+
+      // Remove 'export' keyword from classes, functions, variables etc.
+      sourceFile.getClasses().forEach((c) => c.setIsExported(false));
+
+      // Save the updated source file text in memory
+      outputText += sourceFile.getFullText() + "\n";
+    }
+
+    const outputFileTS = path.join(
+      config.outputDir,
+      path.basename(entryFilePath)
+    );
+
+    // Write the concatenated TypeScript code to the output file
+    fs.writeFileSync(outputFileTS, outputText);
+
+    // Transpile the TypeScript code to JavaScript
+    const outputJS = tsMorph.ts.transpileModule(outputText, {
+      compilerOptions: project.getCompilerOptions(),
+    });
+
+    const outputFileJS = outputFileTS.replace(".ts", ".js");
+
+    // Write the transpiled JavaScript code to the output file
+    fs.writeFileSync(outputFileJS, outputJS.outputText);
   });
-
-  // Write the transpiled JavaScript code to the output file
-  fs.writeFileSync(outputFile, outputJS.outputText);
 
   done();
 };
